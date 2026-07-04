f5ea60ab10d105f2408f639f8def4d9831411e7d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-04 10:47:46 +0100

    Add files via upload

---
+++ store.java
@@ -0,0 +1,13 @@
+public class store {
+    public static void main(String argv[]){
+         int i=10;
+         int ii=0;
+         System.out.println("\033c\033[47;31m\n");
+         ii=i;
+         System.out.println(ii);
+    }
+    public static void start(String argv[]){
+         main(argv);
+    }
+
+}
